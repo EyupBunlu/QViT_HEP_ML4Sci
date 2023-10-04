b3d3dfb2dd0e4bae8d0e8c5c20c3b2f7c84e6521
Eyup Unlu <eyup.unlu@c0307a-s5.ufhpc>
2023-10-04 02:40:38 -0400

    Removed the old pennylane code

--- QViT/pennylane/__init__.py
+++
@@ -1,9 +0,0 @@
-import torch
-from .circuits import *
-from .visual import *
-from .parametrizations import *
-from .model import *
-from .ml_functions import *
-if __name__=='__main__':
-    import sys
-    
